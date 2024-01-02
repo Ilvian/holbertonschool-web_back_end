5f5a6bf1515dbad0f50664d2488d4b923673ce1b
Ilvian <d.ilvian1993@gmail.com>
2024-01-02 03:08:12 -0800

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -14,5 +14,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Function that returns a list of delays
     '''
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
+    delayed_tasks = await asyncio.gather(*tasks)
     return delayed_tasks
